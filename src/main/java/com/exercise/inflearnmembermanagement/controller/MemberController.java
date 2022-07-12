6f0f26931a0b95b151c976b32f8bd652dc58e685
ybkim-dev <beanybigggy@gmail.com>
2022-07-12 19:13:44 +0900

    member controller 작성
    member controller, member repository, member service 에 대해 `Component Scan` 방식을 사용하여 스프링 빈에 등록하는 코드를 작성했습니다.

---
+++ src/main/java/com/exercise/inflearnmembermanagement/controller/MemberController.java
@@ -0,0 +1,16 @@
+package com.exercise.inflearnmembermanagement.controller;
+
+import com.exercise.inflearnmembermanagement.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+}
