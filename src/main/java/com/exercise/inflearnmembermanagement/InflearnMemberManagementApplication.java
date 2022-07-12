5ae655042a7f46b13319bb9d8c8f1bf6d66c06c9
ybkim-dev <beanybigggy@gmail.com>
2022-07-12 16:24:30 +0900

    initial commit

---
+++ src/main/java/com/exercise/inflearnmembermanagement/InflearnMemberManagementApplication.java
@@ -0,0 +1,13 @@
+package com.exercise.inflearnmembermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnMemberManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InflearnMemberManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/exercise/inflearnmembermanagement/InflearnMemberManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.inflearnmembermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnMemberManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
