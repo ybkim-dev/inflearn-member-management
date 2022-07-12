553279b15cdeb370d7fd777ba1dd43e32742c212
ybkim-dev <beanybigggy@gmail.com>
2022-07-12 23:44:34 +0900

    Member api 작성
    회원 가입, 회원 리스트 조회 기능 추가했습니다.

---
+++ src/main/java/com/exercise/inflearnmembermanagement/controller/MemberForm.java
@@ -0,0 +1,13 @@
+package com.exercise.inflearnmembermanagement.controller;
+
+public class MemberForm {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
